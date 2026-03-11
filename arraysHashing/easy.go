9aadd2070ab91426e47819ec28efc161f9c0283c
Faruk Spahic <farukspahictz@gmail.com>
2026-03-11 20:05:48 +0100

    Arrays&Hashing: is anagram

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -13,3 +13,27 @@ func HasDuplicates(numbers []int) bool {
 
 	return false
 }
+
+func IsAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	counts := make(map[rune]int)
+
+	for _, r := range s {
+		counts[r]++
+	}
+
+	for _, r := range t {
+		counts[r]--
+	}
+
+	for _, count := range counts {
+		if count != 0 {
+			return false
+		}
+	}
+
+	return true
+}
