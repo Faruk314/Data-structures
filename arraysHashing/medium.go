84b4033835ab64ccb8497ab365c30107f8021b7d
Faruk Spahic <farukspahictz@gmail.com>
2026-03-13 14:34:26 +0100

    Arrays&Hashing: Top k frequent

--- arraysHashing/medium.go
+++ arraysHashing/medium.go
@@ -27,3 +27,31 @@ func GroupAnagrams(strs []string) [][]string {
 
 	return result
 }
+
+func TopKFrequent(nums []int, k int) []int {
+	freq := map[int]int{}
+
+	for _, num := range nums {
+		freq[num]++
+	}
+
+	buckets := make([][]int, len(nums)+1)
+
+	for num, count := range freq {
+		buckets[count] = append(buckets[count], num)
+	}
+
+	result := []int{}
+
+	for i := len(buckets) - 1; i >= 0 && len(result) < k; i-- {
+		for _, num := range buckets[i] {
+			result = append(result, num)
+
+			if len(result) == k {
+				return result
+			}
+		}
+	}
+
+	return result
+}
