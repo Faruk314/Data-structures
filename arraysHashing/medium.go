120a2b6c7730df468fd9a0b3b06c0701d63b0642
Faruk Spahic <farukspahictz@gmail.com>
2026-03-20 18:19:16 +0100

    Arrays&Hashing: merge sort

--- arraysHashing/medium.go
+++ arraysHashing/medium.go
@@ -125,3 +125,35 @@ func LongestConsecutive(nums []int) int {
 
 	return longest
 }
+
+func SortArray(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	mid := len(nums) / 2
+	left := SortArray(nums[:mid])
+	right := SortArray(nums[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+
+	return result
+}
