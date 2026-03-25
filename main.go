d60b1c6a29f588f587e444ecf193c7d643399081
Faruk Spahic <farukspahictz@gmail.com>
2026-03-25 14:21:45 +0100

    Name change

--- main.go
+++ main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"fmt"
-	arrayshashing "golang/arraysHashing"
-)
-
 func main() {
-	anagrams := []string{"act", "pots", "tops", "cat", "stop", "hat"}
-
-	result := arrayshashing.GroupAnagrams(anagrams)
-
-	fmt.Println(result, "res")
 }
