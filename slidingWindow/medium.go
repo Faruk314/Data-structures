b637f5c3518dbb053ea6e3dffe082bc46e3f13f3
Faruk Spahic <farukspahictz@gmail.com>
2026-03-27 19:21:01 +0100

    SlidingWindow: longest repeating character replacement

--- slidingWindow/medium.go
+++ slidingWindow/medium.go
@@ -31,3 +31,34 @@ func LengthOfLongestSubstring(s string) int {
 
 	return maxLen
 }
+
+func characterReplacement(s string, k int) int {
+	freq := make([]int, 26)
+	left := 0
+	maxFreq := 0
+	maxWindow := 0
+
+	for right := 0; right < len(s); right++ {
+
+		charIdx := s[right] - 'A'
+		freq[charIdx]++
+
+		if freq[charIdx] > maxFreq {
+			maxFreq = freq[charIdx]
+		}
+
+		windowLength := right - left + 1
+
+		if windowLength-maxFreq > k {
+			freq[s[left]-'A']--
+			left++
+		}
+
+		currentWindow := right - left + 1
+		if currentWindow > maxWindow {
+			maxWindow = currentWindow
+		}
+	}
+
+	return maxWindow
+}
