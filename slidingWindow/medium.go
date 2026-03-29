3af75d5c787b1021547efad9433721d07d6f7402
Faruk Spahic <farukspahictz@gmail.com>
2026-03-29 13:59:58 +0200

    SlidingWindow: permutation in a string

--- slidingWindow/medium.go
+++ slidingWindow/medium.go
@@ -62,3 +62,33 @@ func characterReplacement(s string, k int) int {
 
 	return maxWindow
 }
+
+func CheckInclusion(s1 string, s2 string) bool {
+	n1, n2 := len(s1), len(s2)
+	if n1 > n2 {
+		return false
+	}
+
+	var s1Count, s2Count [26]int
+
+	for i := 0; i < n1; i++ {
+		s1Count[s1[i]-'a']++
+		s2Count[s2[i]-'a']++
+	}
+
+	if s1Count == s2Count {
+		return true
+	}
+
+	for i := n1; i < n2; i++ {
+
+		s2Count[s2[i]-'a']++
+		s2Count[s2[i-n1]-'a']--
+
+		if s1Count == s2Count {
+			return true
+		}
+	}
+
+	return false
+}
