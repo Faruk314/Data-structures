5a9aeaecd0b03359787544daae4eb3613ab65c15
Faruk Spahic <farukspahictz@gmail.com>
2026-04-07 19:07:53 +0200

    BinarySearch: Search insert position

--- binarySearch/easy.go
+++ binarySearch/easy.go
@@ -20,3 +20,22 @@ func BinarySearch(nums []int, target int) int {
 
 	return -1
 }
+
+func SearchInsert(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return low
+}
