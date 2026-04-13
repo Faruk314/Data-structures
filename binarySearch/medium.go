1cc08e11f1b1dd53645dcc69aad6847dcb57ef48
Faruk Spahic <farukspahictz@gmail.com>
2026-04-13 15:34:02 +0200

    Binary search: Find minimum in rotated sorted array

--- binarySearch/medium.go
+++ binarySearch/medium.go
@@ -109,3 +109,22 @@ func ShipWithingDays(weights []int, days int) int {
 
 	return minCap
 }
+
+func FindMin(nums []int) int {
+	n := len(nums)
+
+	left := 0
+	right := n - 1
+
+	for left < right {
+		mid := left + (right-left)/2
+
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return nums[left]
+}
