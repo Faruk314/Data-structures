e6fedffb5190187dc885fd2063624d46a6657370
Faruk Spahic <farukspahictz@gmail.com>
2026-04-19 17:57:29 +0200

    BinaryTree: Postorder iterative traversal

--- binaryTree/easy.go
+++ binaryTree/easy.go
@@ -55,3 +55,32 @@ func PreorderTraversal(root *TreeNode) []int {
 
 	return result
 }
+
+func PostorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	var result []int
+	s := stack.Stack[*TreeNode]{}
+	var lastVisited *TreeNode
+	curr := root
+
+	for !s.IsEmpty() || curr != nil {
+		if curr != nil {
+			s.Push(curr)
+			curr = curr.Left
+		} else {
+
+			peekNode, _ := s.Peek()
+
+			if peekNode.Right != nil && lastVisited != peekNode.Right {
+				curr = peekNode.Right
+			} else {
+				result = append(result, peekNode.Val)
+				lastVisited, _ = s.Pop()
+			}
+		}
+	}
+	return result
+}
