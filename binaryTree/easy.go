60fde75a9539271f1cd58aa021cc6b4e6b01f6ec
Faruk Spahic <farukspahictz@gmail.com>
2026-05-11 15:41:26 +0200

    BinaryTree: Subtree of Another Tree

--- binaryTree/easy.go
+++ binaryTree/easy.go
@@ -243,3 +243,19 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 
 	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if subRoot == nil {
+		return true
+	}
+
+	if root == nil {
+		return false
+	}
+
+	if isSameTree(root, subRoot) {
+		return true
+	}
+
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
