baae5c4862929ef9a0314398f742618d6085b914
Faruk Spahic <farukspahictz@gmail.com>
2026-05-18 14:49:34 +0200

    Arrays&Hashing: Is Subsequence

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -117,3 +117,25 @@ func GetConcatenation(nums []int) []int {
 
 	return ans
 }
+
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	if len(s) > len(t) {
+		return false
+	}
+
+	j := 0
+
+	for i := 0; i < len(t); i++ {
+		if t[i] == s[j] {
+			j++
+			if j == len(s) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
