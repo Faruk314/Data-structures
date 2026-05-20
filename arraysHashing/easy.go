9b671247633b58d410a92418c3d10ea955a5f715
Faruk Spahic <farukspahictz@gmail.com>
2026-05-20 20:28:23 +0200

    Arrays&Hashing: Maximum Product Difference Between Two Pairs

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -1,6 +1,7 @@
 package arrayshashing
 
 import (
+	"math"
 	"sort"
 	"strconv"
 	"strings"
@@ -209,4 +210,27 @@ func StringMatching(words []string) []string {
 	}
 
 	return result
+}
+
+func maxProductDifference(nums []int) int {
+	max1, max2 := math.MinInt, math.MinInt
+	min1, min2 := math.MaxInt, math.MaxInt
+
+	for _, num := range nums {
+		if num > max1 {
+			max2 = max1
+			max1 = num
+		} else if num > max2 {
+			max2 = num
+		}
+
+		if num < min1 {
+			min2 = min1
+			min1 = num
+		} else if num < min2 {
+			min2 = num
+		}
+	}
+
+	return (max1 * max2) - (min1 * min2)
 }
