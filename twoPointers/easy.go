feb2d853516d230da70ad066b1065929594c237e
Faruk Spahic <farukspahictz@gmail.com>
2026-05-23 15:11:46 +0200

    TwoPointers: Reverse Words in a String III

--- twoPointers/easy.go
+++ twoPointers/easy.go
@@ -215,3 +215,26 @@ func sortArrayByParity(nums []int) []int {
 
 	return nums
 }
+
+func ReverseWords(s string) string {
+	bytes := []byte(s)
+	start := 0
+
+	for i := 0; i <= len(bytes); i++ {
+		if i == len(bytes) || bytes[i] == ' ' {
+
+			left := start
+			right := i - 1
+
+			for left < right {
+				bytes[left], bytes[right] = bytes[right], bytes[left]
+				left++
+				right--
+			}
+
+			start = i + 1
+		}
+	}
+
+	return string(bytes)
+}
