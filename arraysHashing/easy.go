841bfa9c5f71ea608e8693aa3a27472f6af2ed45
Faruk Spahic <farukspahictz@gmail.com>
2026-05-23 14:29:23 +0200

    Arrays&Hashing: Pascal's Triangle

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -234,3 +234,22 @@ func maxProductDifference(nums []int) int {
 
 	return (max1 * max2) - (min1 * min2)
 }
+
+func GenerateRows(numRows int) [][]int {
+	triangle := make([][]int, numRows)
+
+	for i := 0; i < numRows; i++ {
+		row := make([]int, i+1)
+
+		row[0] = 1
+		row[i] = 1
+
+		for j := 1; j < i; j++ {
+			row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+		}
+
+		triangle[i] = row
+	}
+
+	return triangle
+}
