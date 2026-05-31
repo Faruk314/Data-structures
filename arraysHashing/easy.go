d444badbc550a923a7a7186c33858f5bb2f97e75
Faruk Spahic <farukspahictz@gmail.com>
2026-05-31 16:44:58 +0200

    Arrays&Hashing: Can Place Flowers

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -253,3 +253,31 @@ func GenerateRows(numRows int) [][]int {
 
 	return triangle
 }
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	if n <= 0 {
+		return true
+	}
+
+	for i := 0; i < len(flowerbed); i++ {
+		space := flowerbed[i]
+
+		if space == 1 {
+			continue
+		}
+
+		leftEmpty := i == 0 || flowerbed[i-1] == 0
+		rightEmpty := i == len(flowerbed)-1 || flowerbed[i+1] == 0
+
+		if leftEmpty && rightEmpty {
+			flowerbed[i] = 1
+			n--
+
+			if n == 0 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
