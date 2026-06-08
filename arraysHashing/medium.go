5b2980e9acb0482f1115b840f2cb48a2103fd96a
Faruk Spahic <farukspahictz@gmail.com>
2026-06-08 21:20:19 +0200

    Arrays&Hashing: Largest Number

--- arraysHashing/medium.go
+++ arraysHashing/medium.go
@@ -771,3 +771,20 @@ func sequentialDigits(low int, high int) []int {
 
 	return result
 }
+
+func largestNumber(nums []int) string {
+	strs := make([]string, len(nums))
+	for i, num := range nums {
+		strs[i] = strconv.Itoa(num)
+	}
+
+	sort.Slice(strs, func(i, j int) bool {
+		return strs[i]+strs[j] > strs[j]+strs[i]
+	})
+
+	if strs[0] == "0" {
+		return "0"
+	}
+
+	return strings.Join(strs, "")
+}
