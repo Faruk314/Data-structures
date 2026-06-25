3bc1aa4809c2588acf3314e0d75232b6d9d0e00b
Faruk Spahic <farukspahictz@gmail.com>
2026-06-25 18:31:51 +0200

    Greedy:  Gas Station

--- greedy/medium.go
+++ greedy/medium.go
@@ -293,3 +293,31 @@ func jump(nums []int) int {
 
 	return totalJumps
 }
+
+func canCompleteCircuit(gas []int, cost []int) int {
+	totalGas := 0
+	totalCost := 0
+
+	for i := 0; i < len(gas); i++ {
+		totalGas += gas[i]
+		totalCost += cost[i]
+	}
+
+	if totalCost > totalGas {
+		return -1
+	}
+
+	startIndex := 0
+	currGas := 0
+
+	for i := 0; i < len(gas); i++ {
+		currGas += gas[i] - cost[i]
+
+		if currGas < 0 {
+			currGas = 0
+			startIndex = i + 1
+		}
+	}
+
+	return startIndex
+}
