0edfcd5fe59d4c7157d2a64e6c9c3ca1d4cbc83b
Faruk Spahic <farukspahictz@gmail.com>
2026-06-26 13:05:12 +0200

    Greedy:  Minimum Number of Changes to Make Binary String Beautiful

--- greedy/medium.go
+++ greedy/medium.go
@@ -353,3 +353,15 @@ func isNStraightHand(hand []int, groupSize int) bool {
 
 	return true
 }
+
+func minChanges(s string) int {
+	changes := 0
+
+	for i := 0; i < len(s); i += 2 {
+		if s[i] != s[i+1] {
+			changes++
+		}
+	}
+
+	return changes
+}
