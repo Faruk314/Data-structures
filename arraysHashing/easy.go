fbba25a1fc18eb53c312a8c170a14fd055b33c7c
Faruk Spahic <farukspahictz@gmail.com>
2026-06-29 13:02:38 +0200

    Arrays&Hashing: Replace Elements With Greatest Element On Right Side

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -517,3 +517,18 @@ func validWordSquare(words []string) bool {
 	}
 	return true
 }
+
+func replaceElements(arr []int) []int {
+	max := arr[len(arr)-1]
+	arr[len(arr)-1] = -1
+
+	for i := len(arr) - 2; i >= 0; i-- {
+		if arr[i] > max {
+			arr[i], max = max, arr[i]
+		} else {
+			arr[i] = max
+		}
+	}
+
+	return arr
+}
