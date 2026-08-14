6b6e42c3a55afb047ee2373e30b9b2f471443486
Faruk Spahic <farukspahictz@gmail.com>
2026-08-14 18:36:40 +0200

    Arrays&Hashing: Remove element

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -632,3 +632,19 @@ func countCharacters(words []string, chars string) int {
 
 	return totalLen
 }
+
+func removeElement(nums []int, val int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		if nums[left] == val {
+			nums[left] = nums[right]
+			right--
+		} else {
+			left++
+		}
+	}
+
+	return left
+}
