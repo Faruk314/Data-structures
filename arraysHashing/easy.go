7e9e72d71ed5e5981cd3058d1b2b57ebfc74e856
Faruk Spahic <farukspahictz@gmail.com>
2026-09-01 12:49:30 +0200

    Arrays&Hashing:  Sign of the Product of an Array

--- arraysHashing/easy.go
+++ arraysHashing/easy.go
@@ -1340,3 +1340,20 @@ func uncommonFromSentences(s1 string, s2 string) []string {
 
 	return result
 }
+
+func arraySign(nums []int) int {
+	sign := 1
+
+	for _, num := range nums {
+		if num == 0 {
+			return 0
+		}
+
+		if num < 0 {
+			sign = -sign
+		}
+
+	}
+
+	return sign
+}
