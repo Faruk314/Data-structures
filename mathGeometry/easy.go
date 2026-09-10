a5136727f1e5675391d07ff9e835905a5060d4b0
Faruk Spahic <farukspahictz@gmail.com>
2026-09-10 11:52:21 +0200

    Math&Geometry: Count Odd Numbers in an Interval Range

--- mathGeometry/easy.go
+++ mathGeometry/easy.go
@@ -455,3 +455,15 @@ func totalMoney(n int) int {
 
 	return total
 }
+
+func countOdds(low int, high int) int {
+	count := 0
+
+	for i := low; i <= high; i++ {
+		if i%2 != 0 {
+			count++
+		}
+	}
+
+	return count
+}
