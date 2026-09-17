2209e6defb689fc3e86900f807d5c3f98a290d02
Faruk Spahic <farukspahictz@gmail.com>
2026-09-17 14:46:03 +0200

    Greedy: Two City Scheduling

--- greedy/medium.go
+++ greedy/medium.go
@@ -799,3 +799,22 @@ func repeatLimitedString(s string, repeatLimit int) string {
 
 	return string(result)
 }
+
+func twoCitySchedCost(costs [][]int) int {
+	sort.Slice(costs, func(i, j int) bool {
+		return (costs[i][0] - costs[i][1]) < (costs[j][0] - costs[j][1])
+	})
+
+	totalCost := 0
+	n := len(costs) / 2
+
+	for i := 0; i < n; i++ {
+		totalCost += costs[i][0]
+	}
+
+	for i := n; i < len(costs); i++ {
+		totalCost += costs[i][1]
+	}
+
+	return totalCost
+}
